6e734376a767009923af9bf268e0f14b1d40e612
Mariano Heredia <mariano@devnano.me>
2009-07-13 00:21:16 +0000

    Added class diagrams.
    git-svn-id: https://xp-dev.com/svn/albolocura-WeightTracker/branch/WeightTracker@44 89184dbe-526c-0410-b0ad-bb8eefd3d4d2

--- Classes/DefaultRecipientMailOptionsController.h
+++ Classes/DefaultRecipientMailOptionsController.h
@@ -1,11 +1,3 @@
-//
-//  DefaultRecipientMailOptionsController.h
-//  WeightTracker
-//
-//  Created by Mariano Heredia on 6/27/09.
-//  Copyright 2009 __MyCompanyName__. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 #import <AddressBook/AddressBook.h> 
 #import <AddressBookUI/AddressBookUI.h> 
